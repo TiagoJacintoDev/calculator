1993d67a57a9f708f6084123b291dd93c4cd40fb
Tiago <tiagojacintodev@gmail.com>
2022-06-16 17:10:05 +0200

    Create first version of script

---
+++ script.js
@@ -0,0 +1,19 @@
+const result = document.querySelector('.result');
+const display = document.querySelector('.display');
+const buttons = document.querySelectorAll('button');
+
+function Divide(num1, num2) {
+  return num1 / num2;
+}
+
+function Multiply(num1, num2) {
+  return num1 * num2;
+}
+
+function Subtract(num1, num2) {
+  return num1 - num2;
+}
+
+function Add(num1, num2) {
+  return num1 + num2;
+}
